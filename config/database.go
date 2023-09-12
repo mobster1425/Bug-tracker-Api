145f1a783f0f3b4e876f71dfa04ab68096c0b8e2
feyintola <feyintola33@gmail.com>
2023-09-13 00:06:04 +0530

    updated main.go4

--- config/database.go
+++ config/database.go
@@ -15,27 +15,27 @@ var DB *gorm.DB
 
 func DBconnect() (*gorm.DB, error) {
 
-	uri := os.Getenv("DBURL")
-	DB, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	/*
-		 host := os.Getenv("DB_HOST")
-			port := os.Getenv("DB_PORT")
-		 user := os.Getenv("DB_USER")
-		password := os.Getenv("DB_PASSWORD")
-		dbname := os.Getenv("DB_NAME")
-		uri := "postgres://bug_tracker_api_user:MFxpttTB0B2hiVFyB2BDKOFYee8soeXb@dpg-ck09mah5mpss73ddmvd0-a/bug_tracker_api"
-
-
-		uri := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+		uri := os.Getenv("DBURL")
 		DB, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
 		if err != nil {
 			fmt.Println(err)
 		}
 	*/
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	uri := "postgres://bug_tracker_api_user:MFxpttTB0B2hiVFyB2BDKOFYee8soeXb@dpg-ck09mah5mpss73ddmvd0-a/bug_tracker_api"
+
+	uri := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	DB, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	DB.AutoMigrate(&models.User{})
 	DB.AutoMigrate(&models.Bug{})
 	DB.AutoMigrate(&models.Member{})
