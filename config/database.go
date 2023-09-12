2e4334ab39638a02bbb4e264b76048910c71c4b5
feyintola <feyintola33@gmail.com>
2023-09-13 00:08:00 +0530

    updated the main.go 2

--- config/database.go
+++ config/database.go
@@ -28,7 +28,7 @@ func DBconnect() (*gorm.DB, error) {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
-	uri := "postgres://bug_tracker_api_user:MFxpttTB0B2hiVFyB2BDKOFYee8soeXb@dpg-ck09mah5mpss73ddmvd0-a/bug_tracker_api"
+	// uri := "postgres://bug_tracker_api_user:MFxpttTB0B2hiVFyB2BDKOFYee8soeXb@dpg-ck09mah5mpss73ddmvd0-a/bug_tracker_api"
 
 	uri := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	DB, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
